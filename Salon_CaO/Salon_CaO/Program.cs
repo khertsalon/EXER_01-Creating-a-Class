f3b6f3aab05540ac8122accd04a27db691837849
ACT-STUDENT <ACT-STUDENT@LAB3-PC25>
2025-08-29 14:53:27 +0800

    exer 1

---
+++ Salon_CaO/Salon_CaO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salon_CaO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            personal person1 = new personal();
+
+            person1.name = "Salon Khert";
+            person1.age = 18;
+            person1.gender = "male";
+
+            person1.Greet();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Salon_CaO/Salon_CaO/personal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salon_CaO
+{
+    internal class personal
+    {
+        //Class Members
+        public string name;
+        public int age;
+        public string gender;
+
+
+        //Method Display
+        public void Greet()
+        {
+            Console.WriteLine(" My name is " + name + " and my age is " + age + " My gender is " + gender);
+        }
+
+    }
+}
